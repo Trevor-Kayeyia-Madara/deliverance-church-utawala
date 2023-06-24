94e46a36a1e8586956c5902d5d798d27dfe6864d
Trevor2002-maker <trevor.madara@student.moringaschool.com>
2023-06-24 10:06:59 +0300

    Done

--- src/APP/Components/NavBar/NavBar.jsx
+++ src/APP/Components/NavBar/NavBar.jsx
@@ -18,13 +18,19 @@ const Navbar = () => {
     setIsOpen(false);
   };
 
+  const handleMenuItemClick = () => {
+    if (window.innerWidth <= 960) {
+      setIsOpen(false);
+    }
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="nav-logo">
           <img src={process.env.PUBLIC_URL + "/logo.png"} alt="Deliverance Church Utawala Logo" />
         </div>
-        <Link className="text" to="/">
+        <Link className="text" to="/" onClick={handleMenuItemClick}>
           <h5>Deliverance Church Utawala</h5>
           <p>The Church Of Choice</p>
         </Link>
@@ -33,66 +39,82 @@ const Navbar = () => {
         </div>
         <ul className={isOpen ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-item">
-            <Link to="/Live/livestream" className="nav-links" onClick={toggle}>
+            <Link to="/Live/livestream" className="nav-links" onClick={handleMenuItemClick}>
               Live
             </Link>
           </li>
-          <li className="nav-item dropdown" onMouseEnter={handleDropdownOpen} onMouseLeave={handleDropdownClose}>
+          <li
+            className="nav-item dropdown"
+            onMouseEnter={handleDropdownOpen}
+            onMouseLeave={handleDropdownClose}
+          >
             <div className="nav-links">
               About Us <FaCaretDown />
             </div>
             <ul className={isOpen ? 'dropdown-menu active' : 'dropdown-menu'}>
               <li>
-                <Link to="/About/History" className="dropdown-links" onClick={toggle}>
+                <Link
+                  to="/About/History"
+                  className="dropdown-links"
+                  onClick={handleMenuItemClick}
+                >
                   Our History
                 </Link>
               </li>
               <li>
-                <Link to="/About/Mission/" className="dropdown-links" onClick={toggle}>
+                <Link
+                  to="/About/Mission/"
+                  className="dropdown-links"
+                  onClick={handleMenuItemClick}
+                >
                   Our Mission and Vision
                 </Link>
               </li>
               <li>
-                <Link to="/About/Pastoral" className="dropdown-links" onClick={toggle}>
+                <Link
+                  to="/About/Pastoral"
+                  className="dropdown-links"
+                  onClick={handleMenuItemClick}
+                >
                   Our Pastoral Team
                 </Link>
               </li>
             </ul>
           </li>
           <li className="nav-item">
-            <Link to="/Ministries" className='nav-links' onClick={toggle}>
-            Our Ministries
+            <Link to="/Ministries" className="nav-links" onClick={handleMenuItemClick}>
+              Our Ministries
             </Link>
           </li>
           <li className="nav-item dropdown">
-            <div className="nav-links" onClick={toggle}>
-              Media  <i className="fas fa-caret-down" />
+            <div className="nav-links" onClick={handleDropdownOpen}>
+              Media <FaCaretDown />
             </div>
-            <ul className="dropdown-menu">
+            <ul className={isOpen ? 'dropdown-menu active' : 'dropdown-menu'}>
               <li>
-                <Link to="/sermons/" className="dropdown-links" onClick={toggle}>
+                <Link to="/Sermons/" className="dropdown-links" onClick={handleMenuItemClick}>
                   Latest Sermons
                 </Link>
               </li>
               <li>
-                <Link to="/events/" className="dropdown-links" onClick={toggle}>
-                 Upcoming Events
+                <Link to="/events/" className="dropdown-links" onClick={handleMenuItemClick}>
+                  Upcoming Events
                 </Link>
               </li>
               <li>
-                <Link to="/gallery/" className="dropdown-links" onClick={toggle}>
+                <Link to="/gallery/" className="dropdown-links" onClick={handleMenuItemClick}>
                   Our Gallery
                 </Link>
               </li>
             </ul>
           </li>
           <li className="nav-item">
-            <Link to="/give" className="nav-links" onClick={toggle}>
+            <Link to="/give" className="nav-links" onClick={handleMenuItemClick}>
               Tithes &amp; Offerings
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/contact" className="nav-links" onClick={toggle}>
+            <Link to="/contact" className="nav-links" onClick={handleMenuItemClick}>
               Contact Us
             </Link>
           </li>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './APP';
 
 
