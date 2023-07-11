d60b62f9e6a18f2a0b7da83f1e704cb6cd4adf3a
Trevor2002-maker <trevor.madara@student.moringaschool.com>
2023-07-11 09:24:12 +0300

    Done some changes

--- src/APP/Pages/Home/index.jsx
+++ src/APP/Pages/Home/index.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import BackgroundImage from '../../Components/BackGround/index'
 
 
+
 const Home = () => {
+
+
   return (
     <div>
       <BackgroundImage />
+     
     </div>
   )
 }
