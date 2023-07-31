abd0be07c937ee887597f6cfdf25a1c8ab819311
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2023-07-31 10:46:25 +0300

    Done

--- src/APP/Components/Register/index.jsx
+++ src/APP/Components/Register/index.jsx
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import './style.css';
 
 const AttendeeForm = () => {
-  const backendApiUrl = 'https://register-55xg.onrender.com/';
+  const backendApiUrl = 'https://node-register.onrender.com';
   const [formData, setFormData] = useState({
     title: '',
     first_name: '',
